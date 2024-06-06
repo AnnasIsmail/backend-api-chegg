48a4cd4f6ff29f7820a5afddf0547d9ecc64d268
Anans PC <annasismailmuhammad@gmail.com>
2024-06-06 20:42:41 +0700

    push

--- connectDB.js
+++ connectDB.js
@@ -1,8 +1,7 @@
 
 const db = process.env.DB;
 const mongoose = require("mongoose");
-// mongoose.connect('mongodb://127.0.0.1:27017/Annas',
+
 mongoose.connect(
-    // "mongodb+srv://chegg-permission:chegg123@cluster0.rc4qkvv.mongodb.net/userManagementChegg",
     db,
 );
